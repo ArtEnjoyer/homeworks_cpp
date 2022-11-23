37fae21458f83a799e66e43d5fa5a5124754ad09
ArtEnjoyer <artrussushkof@gmail.com>
2022-11-23 17:54:52 +0300

    test commit

--- homework_01/task_01/src/main.cpp
+++ homework_01/task_01/src/main.cpp
@@ -3,6 +3,7 @@
 #include <utils.hpp>
 
 int main() {
+  cout<<"Tut izmenenie"<<endl;
   for (const auto& word : SplitString("asdas das das fgag (adasd 1fas)")) {
     std::cout << word << "\n";
   }
